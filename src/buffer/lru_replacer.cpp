c4186d940ba35af763f5717bc044feaa430ccc74
Tao liu <129494413+626lt@users.noreply.github.com>
2024-05-16 20:18:53 +0800

    finish lab1 except bonus

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -8,26 +8,40 @@ LRUReplacer::~LRUReplacer() = default;
  * TODO: Student Implement
  */
 bool LRUReplacer::Victim(frame_id_t *frame_id) {
-  return false;
+  if (lru_list_.empty()) {
+    return false;
+  }
+  *frame_id = lru_list_.back();
+  lru_list_.pop_back();
+  lru_list_map_.erase(*frame_id);
+  return true;
 }
 
 /**
  * TODO: Student Implement
  */
 void LRUReplacer::Pin(frame_id_t frame_id) {
-
+  auto it = lru_list_map_.find(frame_id);
+  if(it != lru_list_map_.end()){
+    lru_list_.erase(it->second);
+    lru_list_map_.erase(it);
+  }
 }
 
 /**
  * TODO: Student Implement
  */
 void LRUReplacer::Unpin(frame_id_t frame_id) {
-
+  auto it = lru_list_map_.find(frame_id);
+  if(it == lru_list_map_.end()){
+    lru_list_.push_front(frame_id);
+    lru_list_map_[frame_id] = lru_list_.begin();
+  }
 }
 
 /**
  * TODO: Student Implement
  */
 size_t LRUReplacer::Size() {
-  return 0;
+  return lru_list_.size();
 }
